b859c32e68c658aa802f827da9c585c731b4e1a5
gabriel.souza <gabrielvinicius.sds@gmail.com>
2025-09-26 17:25:51 -0300

    remoção

--- src/shared/config/auth_settings.py
+++
@@ -1,2 +0,0 @@
-# This file is deprecated and can be removed.
-# The AuthSettings class has been moved to src/auth/config/settings.py
--- src/summarization/tasks/tasks.py
+++
@@ -1,2 +0,0 @@
-# This file is obsolete and can be removed.
-# The summarization Celery task has been moved to summary_tasks.py
